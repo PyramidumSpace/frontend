ae161ee642f9787f08ca5f5418b7732b662c6d81
MaxibX <m.nadezhkin@g.nsu.ru>
2024-04-26 17:30:08 +0700

    MANAG-6,10,11: Auth, ResetPassword, vue-router
    Доработана страница авторизации. Добавлены страницы запроса на восстановление пароля и успешного восстановления пароля. Начало использования vue-router и Bootstrap.

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -1,46 +1,80 @@
 <script setup lang="ts">
     import {ref} from 'vue'
-
-    const username = ref('')
-    const password = ref('')
 </script>
 
 <template>
     <div>
-        <h1>{{"Pyramidum"}}</h1>
-        <h2>
-            <em>{{"Пришло время искоренить прокрастинацию..."}}</em>
+        <h1>Pyramidum</h1>
+        <h2 class="slogan">
+            <em>Пришло время искоренить прокрастинацию...</em>
         </h2>
-        <form style="margin-bottom: 5px">
-            <input
-                    placeholder="логин"
-                    v-model="username"
-                    class="input"
-                    type="text"
-            >
-
-        </form>
-        <form style="margin-bottom: 25px">
-            <input
-                    placeholder="пароль"
-                    v-model="password"
-                    class="input"
-                    type="password"
-            >
-        </form>
-        <button style="height: 50px; width: 170px; background-color: darkcyan"
-                @click="console.log(username); console.log(password);">{{"ВХОД"}}
-        </button>
-        <!--Необходима проверка на наличие уч. записи с указанными username и password в БД><-->
-        <p>
-            <button style="margin-right: 5px"
-                    @click="console.log('to register page')">
-                {{"Регистрация"}}
-            </button>
-            <button
-                    @click="console.log('to reset password page')">
-                {{"Забыли пароль?"}}
-            </button>
-        </p>
+        <div>
+            <form @submit.prevent="">
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label fs-6" >Логин:</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" v-model="username" aria-describedby="emailHelp">
+                    <div id="emailHelp" class="form-text">{{username_msg}}</div>
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label fs-6">Пароль:</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" v-model="password" aria-describedby="passwordHelp">
+                    <div id="passwordHelp" class="form-text button-bottom">{{password_msg}}</div>
+                </div>
+                <button @click="this.validation()" type="submit" class="btn btn-success">ВХОД</button>
+            </form>
+        </div>
+        <div>
+            <button @click="this.$router.push('/register')" class="btn btn-secondary">Регистрация</button>
+            <button @click="this.$router.push('/resetpassword')" class="btn btn-secondary">Забыли пароль?</button>
+        </div>
     </div>
 </template>
+
+<script lang="ts">
+    export default {
+        data() {
+            return {
+                username: ref(''),
+                password: ref(''),
+                username_msg: ref(''),
+                password_msg: ref('')
+            }
+        },
+        methods: {
+            validation() {
+                this.username_msg = '';
+                this.password_msg = '';
+                if (this.username == '') {
+                    this.username_msg = 'Пожалуйста, введите логин';
+                    if (this.password == '') {
+                        this.password_msg = 'Пожалуйста, введите пароль';
+                    }
+                }
+                else {
+                    if (this.password == '') {
+                        this.password_msg = 'Пожалуйста, введите пароль';
+                    }
+                    else {
+                        //Запрос к БД. Переход к пользовательскому окну
+                    }
+                }
+            }
+        }
+    }
+</script>
+
+<style>
+    .btn {
+        margin-bottom: 10px;
+        margin-right: 10px;
+    }
+    .slogan {
+        margin-bottom: 50px;
+    }
+    .form-text {
+        color: #dc3545;
+    }
+    .button-bottom {
+        margin-bottom: 50px;
+    }
+</style>
